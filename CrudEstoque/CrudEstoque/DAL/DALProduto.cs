e33d26dc91a65f24f038678821b087082e01a59f
mat.azvd <mat.azvd@gmail.com>
2022-04-07 13:01:16 -0300

    DAL commit

---
+++ CrudEstoque/CrudEstoque/DAL/DALProduto.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CrudEstoque.Modelo;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CrudEstoque.DAL
+{
+    public class DALProduto
+    {
+
+        //Criar uma "ConnectionStringSettings"
+        private System.Configuration.ConnectionStringSettings connString;
+
+        //Metodo construtor para pegar a string de conexão com o banco 
+        public DALProduto()
+        {
+
+            System.Configuration.Configuration rootWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/MyWebSiteRoot");
+            connString = rootWebConfig.ConnectionStrings.ConnectionStrings["ConnectionString"];
+
+        }
+
+        //Metodo "Inserir" novo contato no banco 
+        public void Inserir(ModeloProduto obj)
+        {
+
+            //Criar um objeto de conexão
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connString.ToString();
+            SqlCommand cmd = new SqlCommand();
+
+
+
+            //Executar um comando no banco
+            try
+            {
+                cmd.Connection = con;
+                cmd.CommandText = "Insert into Produto (nome,preco,quantidade,ultima_alt_por) values (@nome,@email,@senha,@ultima_alt_por)";
+                cmd.Parameters.AddWithValue("nome", obj.nome);
+                cmd.Parameters.AddWithValue("preco", obj.preco);
+                cmd.Parameters.AddWithValue("quantidade", obj.quantidade);
+                cmd.Parameters.AddWithValue("ultima_alt_por", obj.ultima_alt_por);
+
+
+                con.Open();
+                obj.ID = Convert.ToInt32(cmd.ExecuteScalar());
+
+            }
+
+            catch (Exception erro)
+            {
+                new Exception(erro.Message);
+            }
+
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void Alterar(ModeloProduto obj)
+        {
+
+            //Criar um objeto de conexão
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connString.ToString();
+            SqlCommand cmd = new SqlCommand();
+
+
+
+            //Executar um comando no banco
+            try
+            {
+                cmd.Connection = con;
+                cmd.CommandText = "update Produto set nome=@nome, preco=@preco, quantidade=@quantidade, ultima_alt_por=@ultima_alt_por where ID=@ID";
+                cmd.Parameters.AddWithValue("ID", obj.ID);
+                cmd.Parameters.AddWithValue("nome", obj.nome);
+                cmd.Parameters.AddWithValue("preco", obj.preco);
+                cmd.Parameters.AddWithValue("quantidade", obj.quantidade);
+                cmd.Parameters.AddWithValue("ultima_alt_por", obj.ultima_alt_por);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+
+            catch (Exception erro)
+            {
+                new Exception(erro.Message);
+            }
+
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        public void Delete(ModeloProduto cod)
+        {
+
+            //Criar um objeto de conexão
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connString.ToString();
+            SqlCommand cmd = new SqlCommand();
+
+
+
+            //Executar um comando no banco
+            try
+            {
+                cmd.Connection = con;
+                cmd.CommandText = "Delete from Produto where ID= " + cod.ToString();
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+
+            }
+
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        public DataTable Listar()
+        {
+            //DataTable para criar uma tabela
+            DataTable tabela = new DataTable();
+
+            //SqlDataAdapter para executar comandos SQL passando o comando e a string de conexao
+            SqlDataAdapter da = new SqlDataAdapter("Select * from Produto", connString.ConnectionString);
+
+            try
+            {
+
+                //Pegar o retorno do comando e preencher a tabela
+                da.Fill(tabela);
+                return tabela;
+            }
+
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+
+
+        }
+
+        /*public DataTable Localizar()
+        {
+            //DataTable para criar uma tabela
+            DataTable tabela = new DataTable();
+
+            //SqlDataAdapter para executar comandos SQL passando o comando e a string de conexao
+            SqlDataAdapter da = new SqlDataAdapter("Select * from contato where con", connString.ConnectionString);
+
+            try
+            {
+
+                //Pegar o retorno do comando e preencher a tabela
+                da.Fill(tabela);
+                return tabela;
+            }
+
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+        
+
+        }
+        */
+
+        public ModeloProduto GetRegistro(String nome)
+        {
+            ModeloProduto obj = new ModeloProduto();
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connString.ToString();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+
+            try
+            {
+                cmd.CommandText = "select * from Produto where nome=@nome";
+                cmd.Parameters.AddWithValue("@nome", nome);
+                con.Open();
+                SqlDataReader registro = cmd.ExecuteReader();
+
+                if (registro.HasRows)
+                {
+                    registro.Read();
+                    obj.ID = Convert.ToInt32(registro["ID"]);
+                    obj.nome = Convert.ToString(registro["nome"]);
+                    obj.preco = Convert.ToInt32(registro["preco"]);
+                    obj.quantidade = Convert.ToInt32(registro["quantidade"]);
+                    obj.ultima_alt_por = Convert.ToInt32(registro["ultima_alt_por"]);
+
+                }
+            }
+
+            catch (Exception erro)
+            {
+
+                throw new Exception(erro.Message);
+            }
+
+            return obj;
+        }
+
+    }
+}
---
+++ CrudEstoque/CrudEstoque/DAL/DALUsuario.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CrudEstoque.Modelo;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace CrudEstoque.DAL
+{
+    public class DALUsuario
+    {
+
+        //Criar uma "ConnectionStringSettings"
+        private System.Configuration.ConnectionStringSettings connString;
+
+        //Metodo construtor para pegar a string de conexão com o banco 
+        public DALUsuario()
+        {
+
+            System.Configuration.Configuration rootWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/MyWebSiteRoot");
+            connString = rootWebConfig.ConnectionStrings.ConnectionStrings["ConnectionString"];
+
+        }
+
+        //Metodo "Inserir" novo contato no banco 
+        public void Inserir(ModeloUsuario obj)
+        {
+
+            //Criar um objeto de conexão
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connString.ToString();
+            SqlCommand cmd = new SqlCommand();
+
+
+
+            //Executar um comando no banco
+            try
+            {
+                cmd.Connection = con;
+                cmd.CommandText = "Insert into Usuario (nome,email,senha) values (@nome,@email,@senha)";
+                cmd.Parameters.AddWithValue("nome", obj.nome);
+                cmd.Parameters.AddWithValue("email", obj.email);
+                cmd.Parameters.AddWithValue("senha", obj.senha);
+
+
+                con.Open();
+                obj.ID = Convert.ToInt32(cmd.ExecuteScalar());
+
+            }
+
+            catch (Exception erro)
+            {
+                new Exception(erro.Message);
+            }
+
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void Alterar(ModeloUsuario obj)
+        {
+
+            //Criar um objeto de conexão
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connString.ToString();
+            SqlCommand cmd = new SqlCommand();
+
+
+
+            //Executar um comando no banco
+            try
+            {
+                cmd.Connection = con;
+                cmd.CommandText = "update Usuario set nome=@nome, email=@email, senha=@senha";
+                cmd.Parameters.AddWithValue("nome", obj.nome);
+                cmd.Parameters.AddWithValue("email", obj.email);
+                cmd.Parameters.AddWithValue("senha", obj.senha);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+
+            catch (Exception erro)
+            {
+                new Exception(erro.Message);
+            }
+
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        public void Delete(ModeloUsuario cod)
+        {
+
+            //Criar um objeto de conexão
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connString.ToString();
+            SqlCommand cmd = new SqlCommand();
+
+
+
+            //Executar um comando no banco
+            try
+            {
+                cmd.Connection = con;
+                cmd.CommandText = "Delete from Usuario where ID= " + cod.ToString();
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+
+            }
+
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        public DataTable Listar()
+        {
+            //DataTable para criar uma tabela
+            DataTable tabela = new DataTable();
+
+            //SqlDataAdapter para executar comandos SQL passando o comando e a string de conexao
+            SqlDataAdapter da = new SqlDataAdapter("Select * from Usuario", connString.ConnectionString);
+
+            try
+            {
+
+                //Pegar o retorno do comando e preencher a tabela
+                da.Fill(tabela);
+                return tabela;
+            }
+
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+
+
+        }
+
+        /*public DataTable Localizar()
+        {
+            //DataTable para criar uma tabela
+            DataTable tabela = new DataTable();
+
+            //SqlDataAdapter para executar comandos SQL passando o comando e a string de conexao
+            SqlDataAdapter da = new SqlDataAdapter("Select * from contato where con", connString.ConnectionString);
+
+            try
+            {
+
+                //Pegar o retorno do comando e preencher a tabela
+                da.Fill(tabela);
+                return tabela;
+            }
+
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+        
+
+        }
+        */
+
+        public ModeloUsuario GetRegistro(String email)
+        {
+            ModeloUsuario obj = new ModeloUsuario();
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connString.ToString();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+
+            try
+            {
+                cmd.CommandText = "select * from Usuario where email=@email";
+                cmd.Parameters.AddWithValue("@email", email);
+                con.Open();
+                SqlDataReader registro = cmd.ExecuteReader();
+
+                if (registro.HasRows)
+                {
+                    registro.Read();
+                    obj.ID = Convert.ToInt32(registro["ID"]);
+                    obj.nome = Convert.ToString(registro["nome"]);
+                    obj.email = Convert.ToString(registro["email"]);
+                    obj.senha = Convert.ToString(registro["senha"]);
+
+                }
+            }
+
+            catch (Exception erro)
+            {
+
+                throw new Exception(erro.Message);
+            }
+
+            return obj;
+        }
+
+    }
+}

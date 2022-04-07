80beaaeff87c5e0effad5de8600ecd75a2e7eb02
mat.azvd <mat.azvd@gmail.com>
2022-04-07 12:37:10 -0300

    Modelos commit

---
+++ CrudEstoque/CrudEstoque/Modelo/ModeloProduto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudEstoque.Modelo
+{
+    public class ModeloProduto
+    {
+        public int ID { get; set; }
+        public String nome { get; set; }
+        public int preco { get; set; }
+        public int quantidade { get; set; }
+        public int ultima_alt_por { get; set; }
+    }
+}
---
+++ CrudEstoque/CrudEstoque/Modelo/ModeloUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudEstoque.Modelo
+{
+    public class ModeloUsuario
+    {
+        public int ID { get; set; }
+        public String nome { get; set; }
+        public String email { get; set; }
+        public String senha { get; set; }
+
+    }
+}
